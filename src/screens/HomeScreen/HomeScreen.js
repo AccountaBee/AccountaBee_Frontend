be00e074eeea69a5fb7d6327e31eead1a6151104
Ashley Valenzuela <ashleyvalenzuelaq@gmail.com>
2020-10-14 12:48:07 -0400

    feat: added sign out and log in

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { useSession } from '../../context'
-import { Text, View } from 'react-native'
+import { Text, View, Button } from 'react-native'
+import { firebase } from '../../firebase/config'
 
 export default function HomeScreen(props) {
   const user = useSession()  
-  console.log('USER', user)
+
+  const signout = () => {
+    return firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        console.log('You are signed out')
+        //setUser(false);
+      });
+  };
+
   return (
         <View>
             <Text>Home Screen</Text>
+            <Button title='Sign Out' onPress={() => signout()}></Button>
         </View>
+
     )
 }
