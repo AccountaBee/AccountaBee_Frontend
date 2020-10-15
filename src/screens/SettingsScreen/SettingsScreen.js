6c1272fc5823829afdcb8885f247f3e576c1fd31
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-15 14:18:55 -0400

    feat: added buttons to settings screen

--- src/screens/SettingsScreen/SettingsScreen.js
+++ src/screens/SettingsScreen/SettingsScreen.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View, Button } from "react-native";
+import CustomButton from "../CustomButton";
+import styles from "./styles";
 
 export default function SettingsScreen(props) {
+	const { navigation } = props;
+
+	const onGoalsPress = () => {
+		navigation.navigate("Goals");
+	};
+
 	return (
-		<View>
-			<Text>Settings Screen</Text>
+		<View style={styles.container}>
+			<Text style={styles.header}>Settings</Text>
+			<CustomButton style={styles.button} title="Change My Goals" onPress={() => onGoalsPress()} />
+			<CustomButton style={styles.button} title="Add Friends" />
+			<CustomButton style={styles.button} title="Update Profile" />
 		</View>
 	);
 }
+
+// need button to route to goal screen
+
+// button for adding a friend
