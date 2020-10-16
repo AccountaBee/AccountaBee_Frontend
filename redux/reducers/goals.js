cf8865b9e181190bcedae4205c3a4c35152a1560
Ashley Valenzuela <ashleyvalenzuelaq@gmail.com>
2020-10-16 11:38:56 -0400

    feat: added reducer for goals

--- redux/reducers/goals.js
+++ redux/reducers/goals.js
@@ -1,22 +1,37 @@
 // import axios from 'axios'
 
+// ---------- ACTION TYPES ---------- //
+const ADD_GOAL = 'ADD_GOAL'
+const GOT_GOALS = 'GOT_GOALS'
+const DELETE_GOAL = 'DELETE_GOAL'
 
-// const ADD_GOAL = 'ADD_GOAL'
-// const GET_GOALS = 'GET_GOAL'
+// ---------- ACTION CREATORS ---------- //
+const gotGoals = goals => ({type: GOT_GOALS, goals})
+const addedGoal = goal => ({type: ADD_GOAL, goal})
+const deletedGoal = ({type: DELETE_GOAL, goal})
 
-// const gotGoals = goals => ({type: GET_GOALS, goals})
+export const getGoals = (id) => async dispatch => {
+  try {
+    const res = await axios.get(`https://skinrx-server.herokuapp.com/api/skintypes/${id}`)
+    dispatch(gotGoals(res.data))
+  } catch (error) {
+    console.log(error)
+  }
+}
 
+export const addGoals = () => {
 
+}
 
-// case GET_GOALS:
-//       return {...state}
-
-//       export const getSkinType = (id) => async dispatch => {
-//         try {
-//           const res = await axios.get(`https://skinrx-server.herokuapp.com/api/skintypes/${id}`)
-//           console.log('dataaaaaaaaaa', res.data)
-//           dispatch(gotSkinType(res.data))
-//         } catch (error) {
-//           console.log(error)
-//         }
-//       }
+  export default function(state = {}, action) {
+    switch (action.type) {
+      case GOT_GOALS:
+        return action.user
+      case ADD_GOAL:
+        return {}
+      case DELETE_GOAL:
+        return {}
+      default:
+        return state
+    }
+  }
