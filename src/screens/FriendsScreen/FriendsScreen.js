efb0c0fb03a26c631ed874aa71fdd28d213025f0
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-18 14:08:23 -0400

    feat: user can confirm a friend request

--- src/screens/FriendsScreen/FriendsScreen.js
+++ src/screens/FriendsScreen/FriendsScreen.js
@@ -7,6 +7,10 @@ import { getRequests } from "../../../redux/reducers/requests";
 import { firebase } from "../../firebase/config";
 import instance from "../../../redux/axios";
 
+//TODO - add loading icon because it takes a second to load requests
+//TODO - make confirm/deny buttons nice icons
+//TODO - better error handling messages on backend
+
 class FriendsScreen extends React.Component {
 	constructor(props) {
 		super(props);
@@ -21,16 +25,29 @@ class FriendsScreen extends React.Component {
 		this.props.getRequests(token);
 	}
 
-	handleChange = text => {
-		this.setState({ email: text });
+	handleChange = email => {
+		this.setState({ email });
 	};
 
 	onRequestPress = async () => {
 		const email = this.state.email;
-		const token = await firebase.auth().currentUser.getIdToken();
 		try {
+			const token = await firebase.auth().currentUser.getIdToken();
 			const res = await instance.post("/friends/request", { token, email });
 			this.props.getSentRequests(token);
+		} catch (error) {
+			alert(error);
+		}
+	};
+
+	onReplyPress = async (status, senderId) => {
+		//need friend id
+		console.log(status, senderId);
+		try {
+			const token = await firebase.auth().currentUser.getIdToken();
+			const res = await instance.put("/friends/reply", { token, senderId, status });
+			console.log(res.data);
+			this.props.getRequests(token);
 		} catch (error) {
 			alert(error);
 		}
@@ -45,7 +62,7 @@ class FriendsScreen extends React.Component {
 				<Text style={styles.header}>My Friends</Text>
 				<Text>Add a Friend! Search your friend's email to add them</Text>
 				<TextInput
-					onChangeText={text => this.handleChange(text)}
+					onChangeText={email => this.handleChange(email)}
 					style={styles.input}
 					placeholder="email"
 					placeholderTextColor="#aaaaaa"
@@ -55,13 +72,17 @@ class FriendsScreen extends React.Component {
 				<Button title="Send Friend Request" onPress={() => this.onRequestPress()} />
 				<Button title="Return to Settings" onPress={() => navigation.navigate("Settings")} />
 				<Text>Friend Requests</Text>
-				<View>
+				<>
 					{requests.map(request => (
-						<Text key={request.uid}>
-							{request.firstName} {request.email}
-						</Text>
+						<View key={request.uid}>
+							<Text>
+								{request.firstName} {request.email}
+							</Text>
+							<Button title="YES" onPress={() => this.onReplyPress("confirmed", request.uid)} />
+							<Button title="NO" onPress={() => this.onReplyPress("denied", request.uid)} />
+						</View>
 					))}
-				</View>
+				</>
 				<Text>Sent Requests</Text>
 				<View>
 					{sentRequests.map(request => (
