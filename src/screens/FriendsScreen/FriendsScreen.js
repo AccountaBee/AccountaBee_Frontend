7e834fc3f83a32bbe3a860ae4944cc16a1ab755b
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-18 12:59:01 -0400

    fix: sent requests render properly

--- src/screens/FriendsScreen/FriendsScreen.js
+++ src/screens/FriendsScreen/FriendsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, TextInput, Button } from "react-native";
 import styles from "./styles";
 import { connect } from "react-redux";
@@ -6,47 +6,62 @@ import { getRequests } from "../../../redux/reducers/requests";
 import { firebase } from "../../firebase/config";
 import instance from "../../../redux/axios";
 
-function FriendsScreen(props) {
-	const { navigation } = props;
+class FriendsScreen extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: ""
+		};
+	}
 
-	const [email, setEmail] = useState("");
+	async componentDidMount() {
+		const token = await firebase.auth().currentUser.getIdToken();
+		this.props.getRequests(token);
+	}
 
-	const onRequestPress = async () => {
+	handleChange = text => {
+		this.setState({ email: text });
+	};
+
+	onRequestPress = async () => {
+		const email = this.state.email;
 		const token = await firebase.auth().currentUser.getIdToken();
 		try {
 			const res = await instance.post("/friends/request", { token, email });
-			props.getRequests(token);
+			this.props.getRequests(token);
 		} catch (error) {
 			alert(error);
 		}
 	};
 
-	console.log(props);
-	return (
-		<View style={styles.container}>
-			<Text style={styles.header}>My Friends</Text>
-			<Text>Add a Friend! Search your friend's email to add them</Text>
-			<TextInput
-				onChangeText={text => setEmail(text)}
-				style={styles.input}
-				placeholder="email"
-				placeholderTextColor="#aaaaaa"
-				underlineColorAndroid="transparent"
-				autoCapitalize="none"
-			/>
-			<Button title="Send Friend Request" onPress={() => onRequestPress()} />
-			<Button title="Return to Settings" onPress={() => navigation.navigate("Settings")} />
-			<Text>Sent Requests</Text>
-			<View>
-				{props.requests &&
-					props.requests.map(request => (
-						<Text>
+	render() {
+		const { navigation } = this.props;
+		const { requests } = this.props || [];
+		return (
+			<View style={styles.container}>
+				<Text style={styles.header}>My Friends</Text>
+				<Text>Add a Friend! Search your friend's email to add them</Text>
+				<TextInput
+					onChangeText={text => this.handleChange(text)}
+					style={styles.input}
+					placeholder="email"
+					placeholderTextColor="#aaaaaa"
+					underlineColorAndroid="transparent"
+					autoCapitalize="none"
+				/>
+				<Button title="Send Friend Request" onPress={() => this.onRequestPress()} />
+				<Button title="Return to Settings" onPress={() => navigation.navigate("Settings")} />
+				<Text>Sent Requests</Text>
+				<View>
+					{requests.map(request => (
+						<Text key={request.uid}>
 							{request.firstName} {request.email}
 						</Text>
 					))}
+				</View>
 			</View>
-		</View>
-	);
+		);
+	}
 }
 
 const mapState = state => ({
