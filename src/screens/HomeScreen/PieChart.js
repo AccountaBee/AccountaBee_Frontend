24139fcd14c003a0567db6e52d14087f206468b8
Alexandra Langton <alexandra.langton@outlook.com>
2020-10-18 17:26:34 -0400

    Feat: goals dash navigates to single goal with payload of 'goal' in route.params on press

--- src/screens/HomeScreen/PieChart.js
+++ src/screens/HomeScreen/PieChart.js
@@ -13,7 +13,12 @@ const graphicColor = ['#8688BC', '#DCDCDC'];
 export default function GoalPieChart(props) {
 	const { completedDays, frequency, title } = props.goal;
 	return (
-		<TouchableOpacity style={pieStyle.container}>
+		<TouchableOpacity
+			style={pieStyle.container}
+			onPress={() =>
+				props.navigation.navigate('Single Goal', { goal: props.goal })
+			}
+		>
 			<View style={pieStyle.textContainer}>
 				<Text style={pieStyle.goalName}>{title}</Text>
 				<Text style={pieStyle.subhead}>
