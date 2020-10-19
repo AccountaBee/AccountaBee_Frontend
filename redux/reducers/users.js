6f8a32a8f9c3f333f7909d0e9f90b7778c15906f
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-19 19:17:21 -0400

    fix: consolidated getUser and login reducer

--- redux/reducers/users.js
+++ redux/reducers/users.js
@@ -1,10 +1,19 @@
 import instance from "../axios";
+import { firebase } from "../../src/firebase/config";
 
 const GOT_USER = "GOT_USER";
 const REMOVE_USER = "REMOVE_USER";
 
 const gotUser = user => ({ type: GOT_USER, user });
 const removeUser = () => ({ type: REMOVE_USER });
+
+export const getUser = () => async dispatch => {
+	try {
+		const token = await firebase.auth().currentUser.getIdToken();
+		const { data } = await instance.post("/users/login", { token });
+		dispatch(gotUser(data));
+	} catch (error) {}
+};
 
 export const registerNewUser = user => async dispatch => {
 	try {
@@ -15,15 +24,14 @@ export const registerNewUser = user => async dispatch => {
 	}
 };
 
-export const login = token => async dispatch => {
-	try {
-		const { data } = await instance.post("/users/login", token);
-
-		dispatch(gotUser(data));
-	} catch (authError) {
-		console.error(authError);
-	}
-};
+// export const login = token => async dispatch => {
+// 	try {
+// 		const { data } = await instance.post("/users/login", token);
+// 		dispatch(gotUser(data));
+// 	} catch (authError) {
+// 		console.error(authError);
+// 	}
+// };
 
 export const logout = () => dispatch => {
 	try {
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
-import GoalPieChart from './PieChart';
-import { connect } from 'react-redux';
-import { getGoalsThunk } from '../../../redux/reducers/goals';
-import styles from './styles';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useEffect } from "react";
+import { Text, View } from "react-native";
+import GoalPieChart from "./PieChart";
+import { connect } from "react-redux";
+import { getGoalsThunk } from "../../../redux/reducers/goals";
+import styles from "./styles";
+import { ScrollView } from "react-native-gesture-handler";
+import { getUser } from "../../../redux/reducers/users";
 
 const pieCalculations = (completedDays, frequency) => {
 	const pieData = [],
@@ -14,10 +15,10 @@ const pieCalculations = (completedDays, frequency) => {
 		let objStr = { x: i, y: 1 };
 		pieData.push(objStr);
 		if (colorComplete) {
-			graphicColor.push('#8688BC');
+			graphicColor.push("#8688BC");
 			colorComplete--;
 		} else {
-			graphicColor.push('#DCDCDC');
+			graphicColor.push("#DCDCDC");
 		}
 	}
 	return [pieData, graphicColor];
@@ -27,6 +28,7 @@ function HomeScreen(props) {
 	useEffect(() => {
 		async function fetchData() {
 			await props.getGoals();
+			await props.getUser();
 		}
 		fetchData();
 	}, []);
@@ -38,43 +40,41 @@ function HomeScreen(props) {
 			</View>
 			<View>
 				<ScrollView>
-        {props.goals.length ? (
-          props.goals.map((goal) => {
-            if (goal.status === 'active') {
-              const [data, graphicColor] = pieCalculations(
-                goal.completedDays,
-                  goal.frequency
-              );
-                return (
-                  <GoalPieChart
-                    graphicColor={graphicColor}
-                    data={data}
-                    key={goal.id}
-                    goal={goal}
-                    navigation={props.navigation}
-                  />
-                );
-              }
-            })
-          ) : (
-          <Text style={styles.noGoals}>
-              You don’t have any goals set!{'\n\n'}Head over to Settings{'\n'}to
-              create some goals and{'\n'}start achieving them.
-          </Text>
-        )}
+					{props.goals.length ? (
+						props.goals.map(goal => {
+							if (goal.status === "active") {
+								const [data, graphicColor] = pieCalculations(goal.completedDays, goal.frequency);
+								return (
+									<GoalPieChart
+										graphicColor={graphicColor}
+										data={data}
+										key={goal.id}
+										goal={goal}
+										navigation={props.navigation}
+									/>
+								);
+							}
+						})
+					) : (
+						<Text style={styles.noGoals}>
+							You don’t have any goals set!{"\n\n"}Head over to Settings{"\n"}to create some goals
+							and{"\n"}start achieving them.
+						</Text>
+					)}
 				</ScrollView>
 			</View>
 		</>
 	);
 }
 
-const mapState = (state) => ({
+const mapState = state => ({
 	user: state.user,
-	goals: state.goals,
+	goals: state.goals
 });
 
-const mapDispatch = (dispatch) => ({
+const mapDispatch = dispatch => ({
 	getGoals: () => dispatch(getGoalsThunk()),
+	getUser: () => dispatch(getUser())
 });
 
 export default connect(mapState, mapDispatch)(HomeScreen);
