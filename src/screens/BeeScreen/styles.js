70ae954f25e3cd1d49b963c2583a4911564e9b38
Alexandra Langton <alexandra.langton@outlook.com>
2020-10-25 15:27:01 -0400

    Style: log in and registration pages

--- src/screens/BeeScreen/styles.js
+++ src/screens/BeeScreen/styles.js
@@ -1,27 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const width = Dimensions.get('window').width;
 
 export default StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: 'center',
-		justifyContent: 'space-between'
+		justifyContent: 'space-between',
 	},
 	header: {
 		flex: 1,
 		height: 120,
-		width: 240,
+		width: '100%',
 		textAlign: 'center',
 		alignSelf: 'center',
 		marginBottom: 40,
 		marginTop: 180,
 		fontFamily: 'Avenir',
 		fontWeight: '500',
-		fontSize: 28,
-		color: '#8688BC'
+		fontSize: 0.08 * width,
+		color: '#8688BC',
 	},
 
 	bee: {
 		width: 50,
-		height: 50
-	}
+		height: 50,
+	},
 });
