8b0a054a91526958a1a50e8a151b13f31cff0bbb
Ashley Valenzuela <ashleyvalenzuelaq@gmail.com>
2020-10-28 20:45:44 -0400

    refactor: restructured variables for deployment

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,10 @@
 import * as firebase from 'firebase';
 import '@firebase/auth';
 import '@firebase/firestore';
-
-const firebaseConfig = {
-	apiKey: process.env.EXPO_API_API_KEY,
-	authDomain: process.env.EXPO_API_AUTH_DOMAIN,
-	databaseURL: process.env.EXPO_API_DATABASE_URL,
-	projectId: process.env.EXPO_API_PROJECT_ID,
-	storageBucket: process.env.EXPO_API_STORAGEBUCKET,
-	appId: process.env.EXPO_API_APP_ID
-};
+import apiKeys from '../../config/keys'
 
 if (!firebase.apps.length) {
-	firebase.initializeApp(firebaseConfig);
+	firebase.initializeApp(apiKeys.firebaseConfig);
 }
 
 export { firebase };
